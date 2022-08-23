d211fdcfd5ff55487bf2029d62066452e64a6547
Ian Darwin <ian@darwinsys.com>
2022-08-23 13:41:06 -0400

    Initial import of course load stuff

---
+++ SetPaths.ps1
@@ -0,0 +1,38 @@
+# PowerShell script to add a set of paths
+# Must run under  set-executionpolicy RemoteSigned or powershell will say not found
+# Update $pathsArray for your needs
+# Consider removing the return statement in the Test-Path clause
+# Author: Ian Darwin based on some pieces found on a web search - no powershell expert am I.
+
+$Today = (Get-Date).DateTime
+Write-Host "SetPaths run on" $Today
+
+$pathsArray = 
+	"C:\Program Files\Eclipse Adoptium\jdk-17.0.3.7-hotspot\bin",
+	"C:\Users\student\apache-maven-3.8.5\bin",
+	"C:\Users\student\bin"
+	"C:\Users\student\flutter\bin"
+
+function Add-To-Path{
+param(
+	[string]$Dir
+)
+
+	if (!(Test-Path $Dir)) {
+		Write-warning "Supplied directory was not found!"
+		return
+	}
+	$PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+	if ($PATH -notlike "*"+$Dir+"*" ) {
+		[Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "Machine")
+		Write-Host "Added" $Dir
+	} else {
+		Write-warning $Dir + "already in path"
+	}
+}
+
+
+foreach ($path in $pathsArray)
+{
+	Add-To-Path $path
+}
---
+++ Setup1.bat
@@ -0,0 +1,59 @@
+REM Androd Module: Installs Part One 
+
+REM Java JDK 11 LTS - needed for older releases 3rd-party apps
+winget install --id EclipseAdoptium.Temurin.11
+REM Java JDK 17 LTS - free from numerous OpenJDK download sites
+winget install --id EclipseAdoptium.Temurin.17
+REM IntelliJ java-only install
+winget install --id JetBrains.IntelliJIDEA.Community
+REM Android Studio IDE (Ch03 to end) - free from Google (it's just JetBrains IntelliJ with extra plugins)
+winget install --id Google.AndroidStudio
+REM Android SDK, tools, emulators - downloaded when starting up Studio first time
+
+
+Flutter SDK - latest - from https://flutter.dev/
+Flutter plugin for Android Studio - via Studio Settings->Plugins
+
+REM Tools used in preparing the course load
+REM git (and bash shell) included in "git bash" 
+winget install --id Git.Git -e
+REM Make (gnu-make) - 
+winget install --id GnuWin32.Make
+REM mitmproxy for Chap 7
+winget install --id mitmproxy.mitmproxy
+REM nmap just for ncat
+winget install --id Insecure.Nmap
+
+echo Downloading maven build tool from https://maven.apache.org/download
+curl -O https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar xzf apache-maven-3.8.6-bin.tar.gz
+
+Set All Browser Home Pages to C:/Users/student/CourseFiles510G/website/index.html
+
+rem Now that files are downloaded, we can set the PATH
+powershell SetPaths.ps1
+
+	C:\Program Files (x86)\mitmproxy\bin
+	%USERPROFILE%\AppData\Local\Android\Sdk\tools\bin
+	%USERPROFILE%\AppData\Local\Android\Sdk\emulator
+	%USERPROFILE%\AppData\Local\Android\Sdk\platform-tools
+	%USERPROFILE%\flutter\bin
+
+REM Documentation
+REM Android Internals book first edition, free download, Used with permission.
+REM SEE http://newandroidbook.com/vault7.htm. 
+curl -o InternalsBookVol1FirstEdn.pdf http://newandroidbook.com/AIvI-M-RL1.pdf
+
+REM Sample Code:
+REM JavaSrc - course author's open-source Java demo package 
+git clone https://github.com/IanDarwin/javasrc
+REM AndroidCookbookExamples - course author's open-source Android demos 
+git clone https://github.com/IanDarwin/Android-Cookbook-Examples
+
+REM Course Problems and Solutions, and a few demos 
+git clone https://github.com/IanDarwin/CourseFiles510G
+REM Chapter 3 Expenses-server - from course author, to upload expenses for Expenses app  
+git clone https://github.com/IanDarwin/expenses-server
+rem Pre-fetch a bunch ("half the internet") of dependencies for Spring-boot server
+mvn -f expenses-server compile
+
