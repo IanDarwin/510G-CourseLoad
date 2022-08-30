76cdda164f018621503375c51ec8ebda06f7cf7c
Ian Darwin <ian@darwinsys.com>
2022-08-30 12:45:46 -0400

    unbreak

--- SetPaths.ps1
+++ SetPaths.ps1
@@ -16,7 +16,6 @@ $pathsArray =
 	"C:\Users\student\AppData\Local\Android\Sdk\platform-tools",
 	"C:\Users\student\flutter\bin",
 	"C:\Program Files (x86)\GnuWin32\bin"
-	"C:\Program Files (x86)\GnuWin32\bin
 
 function Add-To-Path{
 param(
